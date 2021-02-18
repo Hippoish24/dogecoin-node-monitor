955572cbd7bd6b8f8d0ac6af91b7690801570cc7
Hippoish24 <78239042+Hippoish24@users.noreply.github.com>
2021-02-18 14:27:53 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# dogecoin-node-monitor
+# Node Monitor
+
+Node monitor for one or many nodes.  Includes the client script, Prometheus server, Grafana, and frontend webpage.
+
+## Prometheus Client
+
+On port 8334.  Starts a web server with metrics that can be scraped by the Prometheus server.
+metrics are gathered by running `dogecoin-cli` commands every 30 seconds.  Both the port and
+interval are configurable.
+
+Installation:
+```sh
+apt install -y pip3
+pip3 install -r requirements.txt
+```
+
+Running as a background task:
+```sh
+nohup python3 monitor.py &
+```
+
+To kill the task later:
+```sh
+ps ax | grep monitor.py
+kill PID
+```
+
+## Prometheus Server
+
+On port 9090.  Use [prometheus.yml](prometheus-server/prometheus.yml) to configure client
+connections (update the `static_configs` section with the addresses of your nodes).  Can
+also configure the scrape interval (currently 15 seconds).
+
+## Grafana
+
+On port 3030.  Configuration is stored in [grafana.ini](grafana/grafana.ini). Automatically adds
+`http://prometheus:9090` as a data source - if deploying using Docker (see below), this will work
+without any additional configuration.  Also automatically adds `dogecoin.json` as the home
+dashboard, which will connect to the Prometheus data source.
+
+This configuration allows anonymous viewing access and admin authentication.  The admin password
+is stored in [grafana.ini](grafana/grafana.ini) - change it before deploying your server!
+
+There is also commented-out configuration in the `[server]` block if you're serving Grafana as a
+subpage (will require additional reverse proxy configuration that is beyond the scope of this
+project).
+
+Dogecoin dashboard is inspired by this [Bitcoin Node dashboard](https://grafana.com/grafana/dashboards/11274).
+
+
+# Deploying
+
+Deploys using Docker Stack.  The volumes in the [docker-compose.yml](docker-compose.yml) file assume that this directory 
+has been dumped into `/home/rancher` on a RancherOS installation - update the binds as needed.
+
+Run using `docker stack deploy -c docker-compose.yml node_monitor`.
+
+You may need to first init the server as a Docker swarm manager with `docker swarm init --advertise-addr <IP address>`.
---
+++ prometheus-client/monitor.py
@@ -0,0 +1,138 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Modified from https://gist.github.com/ageis/a0623ae6ec9cfc72e5cb6bde5754ab1f
+# and https://github.com/jvstein/bitcoin-prometheus-exporter/blob/master/bitcoind-monitor.py
+# requires pip install prometheus-client
+
+import json
+import time
+import subprocess
+import sys
+from shutil import which
+
+from prometheus_client import start_http_server, Gauge, Counter
+
+# Create Prometheus metrics to track dogecoind stats.
+DOGECOIN_BLOCKS = Gauge('dogecoin_blocks', 'Block height')
+DOGECOIN_HEADERS = Gauge('dogecoin_headers', 'Number of synced headers')
+DOGECOIN_DIFFICULTY = Gauge('dogecoin_difficulty', 'Difficulty')
+DOGECOIN_PEERS = Gauge('dogecoin_peers', 'Number of peers')
+DOGECOIN_HASHPS = Gauge('dogecoin_hashps', 'Estimated network hash rate per second')
+
+DOGECOIN_ERRORS = Counter('dogecoin_errors', 'Number of errors detected')
+DOGECOIN_UPTIME = Gauge('dogecoin_uptime', 'Number of seconds the Bitcoin daemon has been running')
+
+DOGECOIN_MEMPOOL_BYTES = Gauge('dogecoin_mempool_bytes', 'Size of mempool in bytes')
+DOGECOIN_MEMPOOL_SIZE = Gauge('dogecoin_mempool_size', 'Number of unconfirmed transactions in mempool')
+
+DOGECOIN_LATEST_BLOCK_SIZE = Gauge('dogecoin_latest_block_size', 'Size of latest block in bytes')
+DOGECOIN_LATEST_BLOCK_TXS = Gauge('dogecoin_latest_block_txs', 'Number of transactions in latest block')
+
+DOGECOIN_NUM_CHAINTIPS = Gauge('dogecoin_num_chaintips', 'Number of known blockchain branches')
+
+DOGECOIN_TOTAL_BYTES_RECV = Gauge('dogecoin_total_bytes_recv', 'Total bytes received')
+DOGECOIN_TOTAL_BYTES_SENT = Gauge('dogecoin_total_bytes_sent', 'Total bytes sent')
+
+DOGECOIN_LATEST_BLOCK_INPUTS = Gauge('dogecoin_latest_block_inputs', 'Number of inputs in transactions of latest block')
+DOGECOIN_LATEST_BLOCK_OUTPUTS = Gauge('dogecoin_latest_block_outputs', 'Number of outputs in transactions of latest block')
+
+def find_dogecoin_cli():
+    return which('dogecoin-cli')
+
+DOGECOIN_CLI_PATH = str(find_dogecoin_cli())
+
+def dogecoin(cmd):
+    dogecoin = subprocess.Popen([DOGECOIN_CLI_PATH, cmd], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+    output = dogecoin.communicate()[0]
+    return json.loads(output.decode('utf-8'))
+
+
+def dogecoincli(cmd):
+    dogecoin = subprocess.Popen([DOGECOIN_CLI_PATH, cmd], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+    output = dogecoin.communicate()[0]
+    return output.decode('utf-8')
+
+
+def get_block(block_hash):
+    try:
+        block = subprocess.check_output([DOGECOIN_CLI_PATH, 'getblock', block_hash])
+    except Exception as e:
+        print(e)
+        print('Error: Can\'t retrieve block with hash ' + block_hash + ' from dogecoind.')
+        return None
+    return json.loads(block.decode('utf-8'))
+
+
+def get_raw_tx(txid):
+    try:
+        rawtx = subprocess.check_output([DOGECOIN_CLI_PATH, 'getrawtransaction', txid, '1'])
+    except Exception as e:
+        print(e)
+        print('Error: Can\'t retrieve raw transaction ' + txid + ' from dogecoind.')
+        return None
+    return json.loads(rawtx.decode('utf-8'))
+
+
+def refresh_metrics():
+    info = dogecoin('getinfo')
+    chaintips = len(dogecoin('getchaintips'))
+    mempool = dogecoin('getmempoolinfo')
+    nettotals = dogecoin('getnettotals')
+    blockchaininfo = dogecoin('getblockchaininfo')
+    latest_block = get_block(str(blockchaininfo['bestblockhash']))
+    hashps = float(dogecoincli('getnetworkhashps'))
+
+    DOGECOIN_BLOCKS.set(blockchaininfo['blocks'])
+    DOGECOIN_HEADERS.set(blockchaininfo['headers'])
+    DOGECOIN_PEERS.set(info['connections'])
+    DOGECOIN_DIFFICULTY.set(info['difficulty'])
+    DOGECOIN_HASHPS.set(hashps)
+
+    if info['errors']:
+        DOGECOIN_ERRORS.inc()
+
+    DOGECOIN_NUM_CHAINTIPS.set(chaintips)
+
+    DOGECOIN_MEMPOOL_BYTES.set(mempool['bytes'])
+    DOGECOIN_MEMPOOL_SIZE.set(mempool['size'])
+
+    DOGECOIN_TOTAL_BYTES_RECV.set(nettotals['totalbytesrecv'])
+    DOGECOIN_TOTAL_BYTES_SENT.set(nettotals['totalbytessent'])
+
+    if latest_block is not None:
+        DOGECOIN_LATEST_BLOCK_SIZE.set(latest_block['size'])
+        DOGECOIN_LATEST_BLOCK_TXS.set(len(latest_block['tx']))
+        inputs, outputs = 0, 0
+        # counting transaction inputs and outputs requires txindex=1
+        # to be enabled, which may also necessitate reindex=1 in dogecoin.conf
+        for tx in latest_block['tx']:
+
+            rawtx = get_raw_tx(tx)
+            if rawtx is not None:
+                i = len(rawtx['vin'])
+                inputs += i
+                o = len(rawtx['vout'])
+                outputs += o
+
+        DOGECOIN_LATEST_BLOCK_INPUTS.set(inputs)
+        DOGECOIN_LATEST_BLOCK_OUTPUTS.set(outputs)
+
+def main():
+    PORT = 8334
+    TIMEOUT = 30  # Time between refreshes, in seconds
+
+    # Start up the server to expose the metrics.
+    start_http_server(PORT)
+    print(f"Server started on port {PORT}.")
+    while True:
+        try:
+            refresh_metrics()
+        # Rare, but has been seen before, so make sure the server keeps running
+        except TypeError as te:
+            print(repr(te))
+
+        time.sleep(TIMEOUT)
+
+if __name__ == '__main__':
+    main()
